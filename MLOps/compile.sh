3f7e2877f668547dd1ced9f25b6a36b48e00a51c
JacopoRaffi <raffijacopo@gmail.com>
2026-09-18 14:17:38 +0200

    add blas, blis options

--- MLOps/compile.sh
+++ MLOps/compile.sh
@@ -1,9 +1,13 @@
-#!/bin/bash
-
-rm -rf build
-
-mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release ..
-cmake --build . -j$(nproc)
-
+backend="cpu"
+extra_args=()
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --backend) backend="$2"; shift 2 ;;
+        --)        shift; extra_args=("$@"); break ;;
+        *) echo "error: unknown argument '$1'" >&2; exit 1 ;;
+    esac
+done
+rm -rf build && mkdir build && cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DBACKEND="$backend" "${extra_args[@]}" ..
+cmake --build . -j"$(nproc)"
 cd ..
