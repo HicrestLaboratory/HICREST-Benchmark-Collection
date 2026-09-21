89d98068c0aac57c30564da4be8176885a1b330a
Emanuele Venieri <emanuele.venieri2@unibo.it>
2026-09-21 18:20:26 +0200

    expanded install_blas.sh to sapphirerapids, grace, and added generic option

--- common/compile/install_blas.sh
+++ common/compile/install_blas.sh
@@ -16,35 +16,49 @@ if [ "$ARCH" == "sg2044" ]; then
     EPI_LIB="${HOME}/software_env/llvm-EPI/riscv64/rvv1/lib"
     GCC_LIB="/usr/lib/gcc/riscv64-linux-gnu/13/"
 
+    OPENBLAS_MAKE_OPTS="CC=${CC} -B${GCC_LIB} -L${GCC_LIB} -L${EPI_LIB} -I${EPI_INC} NOFORTRAN=1 USE_OPENMP=1 TARGET=$OPENBLAS_TARGET"
+    BLIS_CONFIG_OPTS="CXX=${CXX} CC=${CC} CFLAGS=-I${EPI_INC} LDFLAGS=-B${GCC_LIB} -L${GCC_LIB} -L${EPI_LIB}"
+
 elif [ "$ARCH" == "sg2042" ]; then
     CC="${HOME}/software_env/llvm-EPI/riscv64/rvv071/bin/clang"
     CXX="${HOME}/software_env/llvm-EPI/riscv64/rvv071/bin/clang++"
+
+elif [ "$ARCH" == "grace" ]; then
+    OPENBLAS_TARGET="NEOVERSEV2"
+    BLIS_TARGET="auto"
+
+    OPENBLAS_MAKE_OPTS="USE_OPENMP=1 TARGET=$OPENBLAS_TARGET"
+    BLIS_CONFIG_OPTS=""
+
+elif [ "$ARCH" == "sapphirerapids" ]; then
+    OPENBLAS_TARGET="SAPPHIRERAPIDS"
+    BLIS_TARGET="auto"
+
+    OPENBLAS_MAKE_OPTS="USE_OPENMP=1 TARGET=$OPENBLAS_TARGET"
+    BLIS_CONFIG_OPTS=""
 else
-    echo "Unknown architecture: $ARCH"
-    exit 1
+    echo "No special vars for this architecture"
+    BLIS_TARGET="auto"
+    
+    OPENBLAS_MAKE_OPTS="USE_OPENMP=1"
+    BLIS_CONFIG_OPTS=""
 fi
 
-if [ "$ARCH" == "sg2044" ]; then
-    #OpenBLAS
-    git clone https://github.com/OpenMathLib/OpenBLAS.git
-    cd OpenBLAS
-    git checkout ${OPENBLAS_VERSION}
-    make CC="${CC} -B${GCC_LIB} -L${GCC_LIB} -L${EPI_LIB} -I${EPI_INC}" NOFORTRAN=1 USE_OPENMP=1 TARGET=$OPENBLAS_TARGET -j
-    make install PREFIX="${HOME}/software_env/OpenBLAS-${ARCH}" CC="${CC} -B${GCC_LIB} -L${GCC_LIB} -L${EPI_LIB} -I${EPI_INC}" NOFORTRAN=1 USE_OPENMP=1 TARGET=RISCV64_ZVL128B
-    cd ..
 
-    #BLIS
-    git clone https://github.com/flame/blis.git
-    cd blis
-    git checkout ${BLIS_VERSION}
-    ./configure --prefix=${HOME}/software_env/blis-${ARCH} CXX="${CXX}" CC="${CC}" CFLAGS="-I{EPI_INC}" LDFLAGS="-B${GCC_LIB} -L${GCC_LIB} -L${EPI_LIB}" --enable-multithreading=openmp --enable-cblas ${BLIS_TARGET}
-    make -j
-    make install
-    cd ..
 
-elif [ "ARCH" == "sg2042" ]; then
-    git clone https://github.com/OpenMathLib/OpenBLAS.git
-    cd OpenBLAS
-    git checkout ${OPENBLAS_VERSION}
-    echo "For now nothing"
-fi
+#OpenBLAS
+git clone https://github.com/OpenMathLib/OpenBLAS.git
+cd OpenBLAS
+git checkout ${OPENBLAS_VERSION}
+make ${OPENBLAS_MAKE_OPTS} -j
+make install PREFIX="${HOME}/software_env/OpenBLAS-${ARCH}" ${OPENBLAS_MAKE_OPTS}
+cd ..
+
+#BLIS
+git clone https://github.com/flame/blis.git
+cd blis
+git checkout ${BLIS_VERSION}
+./configure --prefix=${HOME}/software_env/blis-${ARCH} ${BLIS_CONFIG_OPTS} --enable-multithreading=openmp --enable-cblas ${BLIS_TARGET}
+make -j
+make install
+cd ..
